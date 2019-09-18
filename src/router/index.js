a56900055b2e22a130d948b5be29587ce4b26099
0f0 <18841166982@163.com>
2019-09-18 22:58:20 +0800

    登陆页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 Vue.use(Router)
 
 const Login = () => import('@/views/login')
@@ -8,11 +9,23 @@ const Login = () => import('@/views/login')
 const router = new Router({
   mode: 'history',
   routes: [{
-    path: '/',
-    name: 'Login',
-    component: Login
-  }]
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
+  ]
+})
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
 })
 
+router.afterEach(() => {
+  NProgress.done()
+})
 
 export default router
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="login">登录</div>
+  <div class="login">
+    <div class="login-box">
+      <div>
+        <i class="iconfont iconyonghu"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,7 +19,20 @@ export default {
 <style lang="scss" scoped>
 .login {
   height: 100vh;
+  position: relative;
   background: url(~@/assets/login.jpg) no-repeat center center;
   background-size: 100% 100%;
+  .login-box {
+    position: absolute;
+    height: 450px;
+    width: 600px;
+    background: rgba(255, 255, 255, 0.3);
+    top: 50%;
+    right: 0;
+    border-radius: 10px;
+    transform: translate(-25%, -50%);
+    -webkit-transform: translate(-25%, -50%);
+    box-shadow: 1px 10px 10px 1px rgba(0, 0, 0, 0.3); /* no*/
+  }
 }
 </style>
